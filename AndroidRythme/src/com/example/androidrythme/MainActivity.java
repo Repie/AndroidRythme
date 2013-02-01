6c3b2a6919af5a0f738e6cc95672f74ff3cddd51
Rigue <PierreRig@gmail.com>
2013-02-01 13:40:05 +0100

    bolos

--- AndroidRythme/src/com/example/androidrythme/MainActivity.java
+++ AndroidRythme/src/com/example/androidrythme/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.androidrythme;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
+import android.widget.RelativeLayout;
+
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      //on récupère le layout
+        RelativeLayout lay= (RelativeLayout) RelativeLayout.inflate(this, R.layout.activity_main, null);
+        
+        lay.setBackgroundColor(Color.BLUE);
         setContentView(R.layout.activity_main);
+        
+        
     }
 
     @Override
